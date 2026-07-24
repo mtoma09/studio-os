f588d05e4e6126f6425cce8e08d61400995b3f16
mt <manav.tomar@gmail.com>
2026-07-24 11:12:20 +1000

    Delete components directory

--- components/projects/schedules/index.ts
+++
@@ -1,6 +0,0 @@
-export { SchedulesTab } from './SchedulesTab';
-export { ScheduleBuilder } from './ScheduleBuilder';
-export { ScheduleSection } from './ScheduleSection';
-export { ScheduleProductCard } from './ScheduleProductCard';
-export { ProductSidePanel, StatusDropdown } from './ProductSidePanel';
-export { ExportScheduleModal } from './ExportScheduleModal';
